7ebd2301e52c70c71e58b3f4c105863268678c39
MahdiMehrazar <mahdi0993@gmail.com>
2022-07-03 19:39:39 +0530

    updated hardcoded db variables

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 var con = mysql.createConnection({
-  host: "localhost",
+  host: process.env.SQL_HOST,
   user: process.env.SQL_USER,
   password: process.env.SQL_PASS,
-  database: "nodesql"
+  database: process.env.SQL_DATABASE
 });
 
 con.connect(function(err) {
